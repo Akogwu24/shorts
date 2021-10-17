1a3cbc588732f9cd084c557d3be3b49a44bf6aca
Akogwu24 <akogwu.emmanuel24@gmail.com>
2021-10-17 18:42:10 +0100

    camera done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const videoRef = useRef(null);
+  const photoRef = useRef(null);
+  const [hasPhoto, setHasPhoto] = useState(false);
+
+  const getVideo = () => {
+    navigator.mediaDevices
+      .getUserMedia({
+        video: {
+          width: 1920,
+          height: 1080,
+        },
+      })
+      .then((stream) => {
+        let video = videoRef.current;
+        video.srcObject = stream;
+        video.play();
+      })
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    getVideo();
+  }, [videoRef]);
+
   return (
     <div className='App'>
-      <h1>Rawfgg master</h1>
+      <button onClick={getVideo}>Start</button>
+      <div className='camera'>
+        <video style={{ background: 'red' }} ref={videoRef}></video>
+        <button>SNAP!</button>
+      </div>
+      <div className={`result ${hasPhoto ? 'hasPhoto' : ''}`}>
+        <canvas ref={photoRef}></canvas>
+        <button>Close!</button>
+      </div>
     </div>
   );
 }
