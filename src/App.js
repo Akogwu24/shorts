e2b7ac522bcaf5ac56e47a9388b6a80650adc026
akogwu emmanuel <akogwu.emmanuel24@gmail.com>
2021-12-08 16:43:56 +0100

    camera done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import { useEffect, useRef, useState } from 'react';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
 
 function App() {
   const videoRef = useRef(null);
   const photoRef = useRef(null);
   const [hasPhoto, setHasPhoto] = useState(false);
+  const [cameraRunning, setCameraRunning] = useState(false);
 
   const getVideo = () => {
+    setCameraRunning(true);
     navigator.mediaDevices
       .getUserMedia({
         video: {
-          width: 1920,
-          height: 1080,
+          width: 1280,
+          height: 720,
         },
       })
       .then((stream) => {
@@ -21,21 +23,53 @@ function App() {
       })
       .catch((err) => console.error(err));
   };
+  const takePhoto = () => {
+    const width = 414;
+    const height = width / (16 / 9);
+    let video = videoRef.current;
+    let photo = photoRef.current;
+    photo.width = width;
+    photo.height = height;
+    let ctx = photo.getContext("2d");
+    ctx.drawImage(video, 0, 0, width, height);
+    setHasPhoto(true);
+  };
+  const stopCamera = () => {
+    closePhoto();
+    setHasPhoto(false);
+    setCameraRunning(false);
+    // let video = document.querySelector("video");
+    // console.log("query selector", video);
+  };
 
-  useEffect(() => {
-    getVideo();
-  }, [videoRef]);
+  const closePhoto = () => {
+    let photo = photoRef.current;
+    let ctx = photo.getContext("2d");
+    ctx.clearRect(0, 0, photo.width, photo.height);
+    setHasPhoto(false);
+  };
+
+  const retakePhoto = () => {
+    closePhoto();
+    takePhoto();
+  };
 
   return (
-    <div className='App'>
+    <div className="App">
       <button onClick={getVideo}>Start</button>
-      <div className='camera'>
-        <video style={{ background: 'red' }} ref={videoRef}></video>
-        <button>SNAP!</button>
+      <div className="camera">
+        {cameraRunning && (
+          <video style={{ background: "red" }} ref={videoRef}></video>
+        )}
+        <button onClick={takePhoto}>SNAP!</button>
       </div>
-      <div className={`result ${hasPhoto ? 'hasPhoto' : ''}`}>
-        <canvas ref={photoRef}></canvas>
-        <button>Close!</button>
+      <div className={`result ${hasPhoto ? "hasPhoto" : ""}`}>
+        <canvas style={{ background: "hotpink" }} ref={photoRef}>
+          your picture will apear here
+        </canvas>
+        <button onClick={closePhoto}>Close!</button>
+        <button onClick={retakePhoto}>Retake Photo</button>
+        <button onClick={stopCamera}>Stop Camera</button>
       </div>
     </div>
   );
